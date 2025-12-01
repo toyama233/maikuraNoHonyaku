292ee1425b9bdd308890e67015784125832687b1
toyama233 <tensoku.moriya@gmail.com>
2025-12-02 01:47:51 +0900

    AutoTranslator
    自動保存の間隔を1つの変数だけつかうように変更

--- maikuraNoYatu/1.21.1/neoforge/src/main/java/com/example/autotranslator/AutoTranslator.java
+++ maikuraNoYatu/1.21.1/neoforge/src/main/java/com/example/autotranslator/AutoTranslator.java
@@ -13,8 +13,7 @@ import net.neoforged.fml.common.Mod;
 public class AutoTranslator {
     private static boolean sending = true;
     private static int tickCount = 0;
-    private final int SAVE_INTERVAL = 300;
-    private final int TICKS_PER_SECOND = 20;
+    private final int AUTOSAVE_TICKS  = 20 * 300;
 
     //    @SubscribeEvent
 //    public static void onChat(ClientChatReceivedEvent event) {
@@ -61,7 +60,7 @@ public class AutoTranslator {
     @SubscribeEvent
     public static void onServerTick(ServerTickEvent event) {
         tickCount++;
-        if (tickCount % (TICKS_PER_SECOND * SAVE_INTERVAL) == 0) {
+        if (tickCount % (AUTOSAVE_TICKS) == 0) {
             TranslationCache.save();
         }
     }
